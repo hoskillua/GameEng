803299a1c509c3ee25ca1484b972d843f3805c2e
osamamagdy <osamamagdy174@gmail.com>
2022-05-14 23:38:00 +0200

    req 2 done

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -5,12 +5,27 @@
 
 namespace our {
 
-    // This function computes and returns a matrix that represents this transform
-    // Remember that the order of transformations is: Scaling, Rotation then Translation
-    // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
+    /**
+     * @brief 
+     * This function computes and returns a matrix that represents this transform
+     * Remember that the order of transformations is: Scaling, Rotation then Translation
+     * HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
+     * We know that each transformation have a representation in glm to be mat4
+     * Just pass it to those functions with 1.0f as the 4th dimentsion
+     * 
+     * @return glm::mat4 
+     */
     glm::mat4 Transform::toMat4() const {
         //TODO: (Req 2) Write this function
-        return glm::mat4(1.0f);
+
+
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * 
+                              glm::yawPitchRoll(rotation[1],rotation[0],rotation[2]) *
+                              glm::scale(glm::mat4(1.0f),scale);
+
+
+
+        return transform;
     }
 
      // Deserializes the entity data and components from a json object
